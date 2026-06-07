8b67558fe568bd7b3c5973c6ef036455cd604d1f
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2026-06-07 19:24:51 -0300

    Plotagem: Altera mensagem do eixo X na plotagem

--- Plotagem/plotar.m
+++ Plotagem/plotar.m
@@ -6,12 +6,12 @@ figure;
 hold on; 
 grid on; 
 
-plot(dados.Elemento, dados.Comparacoes_FilaSimples, 'r', 'LineWidth', 2);
+plot(dados.Elemento, dados.Comparacoes_Fila, 'r', 'LineWidth', 2);
 
 plot(dados.Elemento, dados.Comparacoes_Heap, 'g', 'LineWidth', 2);  
 
 title('Desempenho de Remoção: Fila Simples vs Max-Heap');
-xlabel('Número de Elementos Restantes na Estrutura');
+xlabel('Tamanho');
 ylabel('Quantidade de Comparações Feitas');
 
 legend('Fila Simples (O(n))', 'Fila com Heap (O(log n))', 'Location', 'northwest');
