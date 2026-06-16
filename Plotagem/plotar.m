c0c91023e37dc24e00be74b86daa95f2314fd22e
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2026-06-16 10:13:47 -0300

    fix: altera mensagem da plotagem

--- Plotagem/plotar.m
+++ Plotagem/plotar.m
@@ -1,14 +1,14 @@
 dados = readtable('dados_comparacao.csv');
 
-dados = sortrows(dados, 'Elemento');
+dados = sortrows(dados, 'Quantidade_de_Elementos');
 
 figure;
 hold on; 
 grid on; 
 
-plot(dados.Elemento, dados.Comparacoes_Fila, 'r', 'LineWidth', 2);
+plot(dados.Quantidade_de_Elementos, dados.Comparacoes_Fila, 'r', 'LineWidth', 2);
 
-plot(dados.Elemento, dados.Comparacoes_Heap, 'g', 'LineWidth', 2);  
+plot(dados.Quantidade_de_Elementos, dados.Comparacoes_Heap, 'g', 'LineWidth', 2);  
 
 title('Desempenho de Remoção: Fila Simples vs Max-Heap');
 xlabel('Tamanho');
